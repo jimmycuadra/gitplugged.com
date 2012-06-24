c3aceab59cdcda18f74cd6fc70460f419973e507
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-23 18:40:12 -0700

    Pass data to JS on homepage.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Repo.create(name: "rails/rails", vote_sum: 1.0, week_start: Date.today.beginning_of_week)
+Repo.create(name: "jquery/jquery", vote_sum: 15.0, week_start: 1.week.ago.beginning_of_week)
+Repo.create(name: "intridea/omniauth", vote_sum: 1.0, week_start: Date.today.beginning_of_week)
