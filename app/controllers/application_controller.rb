e428233af1f8112bc4e2007ba691754bdd78e392
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-23 17:13:05 -0700

    Add current_user method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :logged_in?
+  helper_method :logged_in?, :current_user
 
   private
 
   def logged_in?
     !session[:user_id].nil?
+  end
+
+  def current_user
+    return unless logged_in?
+
+    @current_user ||= User.find(session[:user_id])
   end
 end
