df8b317af5e06439bc3d6ea7d911e08817fd9277
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-23 18:47:01 -0700

    Render client side templates with data from server.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,18 @@
 //= require_tree ./templates
 
 $(function () {
+  var $repos = $("#repos"),
+      $winners = $("#winners");
+
+  _.each(GP.repos, function (repo) {
+    var rendered = JST["templates/repo"](repo);
+
+    $repos.append(rendered);
+  });
+
+  _.each(GP.winners, function (repo) {
+    var rendered = JST["templates/repo"](repo);
+
+    $winners.append(rendered);
+  });
 });
