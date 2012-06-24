a921fb216485f718e8f59c4a97e95115b78a4165
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-24 03:10:37 -0700

    Clean up controller logic.

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -2,37 +2,33 @@ class ReposController < ApplicationController
 
   def index
     @winners = 3.times.map do |i|
-      offset     = i + 1
-      week_start = offset.weeks.ago.beginning_of_week
-      (Repo.where(:week_start => week_start).order("vote_sum DESC").limit(1)).first
+      offset = i + 1
+      query = Repo.where(week_start: offset.weeks.ago.beginning_of_week).order("vote_sum DESC").limit(1)
+      query.first
     end.compact
 
-    @repos = Repo.where(:week_start => Date.today.beginning_of_week).order("vote_sum DESC")
+    @repos = Repo.where(week_start: Date.today.beginning_of_week).order("vote_sum DESC")
   end
 
   def create
-    return render json: {message: "Duplicate repo"}, status: :conflict if Repo.find_by_name(params[:repo][:name]).present?
+    if Repo.find_by_name(params[:repo][:name]).present?
+      return render json: { message: "GitHub repository has already been nominated."}, status: :conflict
+    end
 
     begin
       Octokit.repo(params[:repo][:name])
-    rescue Octokit::NotFound => e
-      return render json: {message: "GitHub repository doesn't exist"}, status: :conflict
+    rescue Octokit::NotFound
+      return render json: { message: "No GitHub repository found with that name." }, status: :conflict
     end
 
-    klout_score_of_user = current_user.klout_score
+    repo = Repo.create!(params[:repo].merge(
+      week_start: Date.today.beginning_of_week,
+    ))
 
-    @repo = Repo.new(params[:repo].merge(
-                       :week_start => Date.today.beginning_of_week,
-                       :vote_sum   => klout_score_of_user
-                     ))
-    @repo.save!
-    @vote = Vote.new(:repo => @repo, :user => current_user, :value => klout_score_of_user)
-    @vote.save!
+    Vote.create!(repo: repo, user: current_user, value: current_user.klout_score)
 
+    repo.reload
 
-    respond_to do |format|
-      format.html { redirect_to @repo, notice: 'Repo was successfully created.' }
-      format.json { render json: @repo, status: :created, location: @repo }
-    end
+    respond_with repo, status: :created
   end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,21 +3,22 @@ class VotesController < ApplicationController
 
   def create
     klout_score = current_user.klout_score
+
     repo = Repo.find_by_name(params[:vote].delete(:repo_name))
-    @vote       = Vote.new(params[:vote].merge(
-                             :repo => repo,
-                             :user  => current_user,
-                             :value => klout_score
-                           ))
-    @vote.save!
 
-    repo.vote_sum += klout_score
-    repo.save!
+    vote = Vote.new(params[:vote].merge(
+      repo: repo,
+      user: current_user,
+      value: klout_score,
+    ))
 
-    if @vote.save
+    if vote.save
+      repo.vote_sum += klout_score
+      repo.save!
+
       respond_with repo, status: :created
     else
-      respond_with @vote.errors, status: :unprocessable_entity
+      respond_with vote.errors, status: :unprocessable_entity
     end
   end
 end
