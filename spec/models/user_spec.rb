841cd0b971dcf0b5328f532831b8ea15390aa97a
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-25 01:31:43 -0700

    Add examples to User spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,29 @@
 require "spec_helper"
 
 describe User do
+  let(:user) do
+    user = User.new
+    user.twitter_uid = "abcdefg"
+    user.name = "Joe Blow"
+    user
+  end
+
+  it "requires a twitter_uid" do
+    user.twitter_uid = nil
+    user.valid?
+    user.should have(1).error_on(:twitter_uid)
+  end
+
+  it "requires a name" do
+    user.name = nil
+    user.valid?
+    user.should have(1).error_on(:name)
+  end
+
+  it "has many votes" do
+    user.votes.should be_an(Array)
+  end
+
   describe ".create_with_omniauth" do
     let(:auth) do
       {
@@ -26,6 +49,22 @@ describe User do
       expect do
         User.create_with_omniauth(bad_auth)
       end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  describe "#klout_score" do
+    it "returns the Klout score if the user is connected to Klout via Twitter" do
+      klout_user = mock("Klout::User", score: { "score" => 1.23 })
+      Klout::Identity.stub(find_by_twitter_id: { "id" => "abcdefg" })
+      Klout::User.stub(new: klout_user)
+
+      user.klout_score.should == 1.23
+    end
+
+    it "returns 1.0 if the user is not connected to Klout via Twitter" do
+      Klout::Identity.stub(:find_by_twitter_id) { raise Klout::NotFound }
+
+      user.klout_score.should == 1.0
     end
   end
 end
