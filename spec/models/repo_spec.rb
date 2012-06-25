dd7f4ae2cde8687302b1e1c21c7f9d58fb84dad5
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-25 01:15:40 -0700

    Add examples to Repo spec.

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,4 +1,33 @@
 require "spec_helper"
 
 describe Repo do
+  let(:repo) { Repo.new(name: "foo/bar", vote_sum: 1.0, week_start: Date.today.beginning_of_week) }
+
+  it "requires a name" do
+    repo.name = nil
+    repo.valid?
+    repo.should have(1).error_on(:name)
+  end
+
+  it "requires a vote_sum" do
+    repo.vote_sum = nil
+    repo.valid?
+    repo.should have(1).error_on(:vote_sum)
+  end
+
+  it "requires a week_start" do
+    repo.week_start = nil
+    repo.valid?
+    repo.should have(1).error_on(:week_start)
+  end
+
+  it "has a unique name" do
+    repo.save
+    duplicate = Repo.create(name: "foo/bar", vote_sum: 2.0, week_start: 1.week.ago.beginning_of_week)
+    duplicate.should have(1).error_on(:name)
+  end
+
+  it "has many votes" do
+    repo.votes.should be_an(Array)
+  end
 end
