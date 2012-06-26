441a214304faae0a3aeb9ce5df66db21c3e1a029
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-25 21:51:05 -0700

    Move logic from ReposController to Repo model.

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,37 +1,16 @@
 class ReposController < ApplicationController
-
   def index
-    @winners = 3.times.map do |i|
-      offset = i + 1
-      query = Repo.where(week_start: offset.weeks.ago.beginning_of_week).order("vote_sum DESC").limit(1)
-      query.first
-    end.compact
-
-    @repos = Repo.where(week_start: Date.today.beginning_of_week).order("vote_sum DESC")
+    @winners = Repo.recent_winners
+    @repos = Repo.in_the_running
   end
 
   def create
-    if Repo.find_by_name(params[:repo][:name]).present?
-      return render json: { message: "GitHub repository has already been nominated."}, status: :conflict
-    end
-
     begin
-      Octokit.repo(params[:repo][:name])
-    rescue Octokit::NotFound
-      return render json: { message: "No GitHub repository found with that name." }, status: :conflict
+      respond_with Repo.nominate(params[:repo], current_user)
+    rescue Repo::AlreadyNominated
+      render json: { message: "GitHub repository has already been nominated." }, status: :conflict
+    rescue Repo::NotFound
+      render json: { message: "No GitHub repository found with that name." }, status: :conflict
     end
-
-    klout_score = current_user.klout_score
-
-    repo = Repo.create!(params[:repo].merge(
-      week_start: Date.today.beginning_of_week,
-      vote_sum: klout_score,
-    ))
-
-    Vote.create!(repo: repo, user: current_user, value: klout_score)
-
-    repo.reload
-
-    respond_with repo, status: :created
   end
 end
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,4 +1,7 @@
 class Repo < ActiveRecord::Base
+  class AlreadyNominated < StandardError; end
+  class NotFound < StandardError; end
+
   attr_accessible :name, :vote_sum, :week_start
 
   has_many :votes
@@ -6,4 +9,38 @@ class Repo < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :vote_sum, presence: true
   validates :week_start, presence: true
+
+  def self.recent_winners
+    3.times.map do |i|
+      offset = i + 1
+      query = where(week_start: offset.weeks.ago.beginning_of_week).order("vote_sum DESC").limit(1)
+      query.first
+    end.compact
+  end
+
+  def self.in_the_running
+    where(week_start: Date.today.beginning_of_week).order("vote_sum DESC")
+  end
+
+  def self.nominate(repo_attributes, user)
+    raise AlreadyNominated if Repo.find_by_name(repo_attributes[:name]).exist?
+
+    begin
+      Octokit.repo(params[:repo][:name])
+    rescue Octokit::NotFound
+      raise NotFound
+    end
+
+    score = user.klout_score
+
+    repo = create!(
+      name: repo_attributes[:name],
+      week_start: Date.today.beginning_of_week,
+      vote_sum: klout_score,
+    )
+
+    Vote.create!(repo: repo, user: current_user, value: score)
+
+    repo
+  end
 end
