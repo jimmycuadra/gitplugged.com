c5113811a426bc3840efdd82c5b5ae0db33130fa
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-25 22:50:27 -0700

    Add examples for ReposController.

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,16 +1,20 @@
 class ReposController < ApplicationController
+  respond_to :json
+
   def index
     @winners = Repo.recent_winners
     @repos = Repo.in_the_running
+
+    respond_to { |format| format.html }
   end
 
   def create
     begin
-      respond_with Repo.nominate(params[:repo], current_user)
+      respond_with Repo.nominate(params[:repo], current_user), status: :created, location: nil
     rescue Repo::AlreadyNominated
       render json: { message: "GitHub repository has already been nominated." }, status: :conflict
     rescue Repo::NotFound
-      render json: { message: "No GitHub repository found with that name." }, status: :conflict
+      render json: { message: "No GitHub repository found with that name." }, status: :unprocessable_entity
     end
   end
 end
--- spec/controllers/repos_controller_spec.rb
+++ spec/controllers/repos_controller_spec.rb
@@ -1,4 +1,37 @@
 require "spec_helper"
 
 describe ReposController do
+  describe "#index" do
+    it "assigns @winners" do
+      Repo.stub(recent_winners: "winners")
+      get :index
+      assigns(:winners).should == "winners"
+    end
+
+    it "assigns @repos" do
+      Repo.stub(in_the_running: "repos")
+      get :index
+      assigns(:repos).should == "repos"
+    end
+  end
+
+  describe "#create" do
+    it "responds with a Repo if nomination is successful" do
+      Repo.stub(:nominate)
+      post :create, format: "json"
+      response.code.should == "201"
+    end
+
+    it "responds with an error if the Repo was already nominated" do
+      Repo.stub(:nominate) { raise Repo::AlreadyNominated }
+      post :create, format: "json"
+      response.code.should == "409"
+    end
+
+    it "responds with an error if the Repo doesn't exist" do
+      Repo.stub(:nominate) { raise Repo::NotFound }
+      post :create, format: "json"
+      response.code.should == "422"
+    end
+  end
 end
