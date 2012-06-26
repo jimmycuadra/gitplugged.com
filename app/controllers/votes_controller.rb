5558a6b5b79d0606d63b74bde0e5c181ebe64aa4
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-25 22:00:11 -0700

    Move logic from VotesController to Vote model.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,21 +2,10 @@ class VotesController < ApplicationController
   respond_to :json
 
   def create
-    klout_score = current_user.klout_score
-
-    repo = Repo.find_by_name(params[:vote].delete(:repo_name))
-
-    vote = Vote.new(params[:vote].merge(
-      repo: repo,
-      user: current_user,
-      value: klout_score,
-    ))
+    vote = Vote.for_repo_by_user(params[:vote].delete(:repo_name), current_user)
 
     if vote.save
-      repo.vote_sum += klout_score
-      repo.save!
-
-      respond_with repo, status: :created
+      respond_with vote.repo, status: :created
     else
       respond_with vote.errors, status: :unprocessable_entity
     end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,7 +4,21 @@ class Vote < ActiveRecord::Base
   belongs_to :repo
   belongs_to :user
 
+  after_create :add_value_to_repo
+
   validates :repo_id, presence: true, uniqueness: { scope: :user_id }
   validates :user_id, presence: true
   validates :value, presence: true
+
+  def self.for_repo_by_user(repo_name, user)
+    repo = Repo.find_by_name!(repo_name)
+
+    repo.votes.build(user: user, value: user.klout_score)
+  end
+
+  private
+
+  def add_value_to_repo
+    vote.repo.update_attribute!(:vote_sum, vote.repo.vote_sum + vote.value)
+  end
 end
