de93d809657a5a951dd316480b3429658d9f5441
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-07-03 00:55:37 -0700

    Add factory definitions.

---
+++ spec/factories.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  factory :repo do
+    name "foo/bar"
+    vote_sum 1.0
+    week_start { Date.today.beginning_of_week }
+  end
+
+  factory :user do
+    name "Joe Blow"
+    twitter_uid "abcdefg"
+  end
+
+  factory :vote do
+    user
+    repo
+
+    value 30.0
+  end
+end
