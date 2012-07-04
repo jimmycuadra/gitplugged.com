76ea0b069d0dc0d808a694914c271c217ab93fb4
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-07-04 03:20:47 -0700

    Add examples to SessionsController spec.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,39 @@
 require "spec_helper"
 
 describe SessionsController do
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe "#create" do
+    before do
+      request.env["omniauth.auth"] = { "uid" => user.twitter_uid }
+
+      get :create
+    end
+
+    it "logs the user in" do
+      controller.send(:logged_in?).should be_true
+    end
+
+    it "redirects to the root with a notice" do
+      response.should redirect_to(root_url)
+      flash[:notice].should include("logged in")
+    end
+  end
+
+  describe "#destroy" do
+    before do
+      session[:uid] = user.id
+
+      get :destroy
+    end
+
+    it "logs the user out" do
+      controller.send(:logged_in?).should_not be_true
+    end
+
+    it "redirects to the root with a notice" do
+      response.should redirect_to(root_url)
+      flash[:notice].should include("logged out")
+    end
+  end
 end
